446c9a65cb9bc133ff4bd38b115eef009073aa8f
chrisliew <chriswvliew@gmail.com>
2019-01-17 19:52:33 +0700

    serve static files from react front end

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.use('/', (req, res) => {
   res.json('Hello World');
 });
 
+const path = require('path');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server listening on port 5000'));
