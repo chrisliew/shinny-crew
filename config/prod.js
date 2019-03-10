e7a954d5f3c16ad07016242d070ae00519290b5f
chrisliew <chriswvliew@gmail.com>
2019-03-09 18:31:15 -0800

    changed keys for stripe prod

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripePrivateKey: process.env.STRIPE_PRIVATE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   facebookAppID: process.env.FACEBOOK_APP_ID,
   facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
   mongoURL: process.env.MONGO_URL
