0fca01c6683e81519339b29afd956a1ab1be7e1a
chrisliew <chriswvliew@gmail.com>
2019-03-20 16:40:49 -0700

    changed proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,25 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
-  app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
-  app.use(proxy('/auth/facebook', { target: 'http://localhost:5000' }));
+  app.use(
+    proxy('/api/*', {
+      target: 'http://localhost:5000',
+      secure: false,
+      changeorigin: true
+    })
+  );
+  app.use(
+    proxy('/auth/google', {
+      target: 'http://localhost:5000',
+      secure: false,
+      changeorigin: true
+    })
+  );
+  app.use(
+    proxy('/auth/facebook', {
+      target: 'http://localhost:5000',
+      secure: false,
+      changeorigin: true
+    })
+  );
 };
