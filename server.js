2162170397395a5f7e4ec82a52e3eef01750156c
chrisliew <chriswvliew@gmail.com>
2019-04-07 06:57:00 -0700

    https forwarding

--- server.js
+++ server.js
@@ -21,8 +21,14 @@ app.use(
   })
 );
 
-app.get('*', (req, res) => {
-  res.redirect('https://' + req.headers.host + req.url);
+app.use(function(req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
 });
 
 app.use(function(req, res, next) {
