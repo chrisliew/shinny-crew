b71a4716eb64d1181a51b46da36358f88a2f088e
chrisliew <chriswvliew@gmail.com>
2019-05-21 05:47:14 -0700

    fixed merge

--- server.js
+++ server.js
@@ -61,7 +61,6 @@ require('./routes/api/email-routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  // like our main.js file, or main.css file
   app.use(express.static('client/build'));
 
   // Express will serve up index.html file
